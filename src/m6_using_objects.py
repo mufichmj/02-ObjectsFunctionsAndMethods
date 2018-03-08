c7896113f651b41b6467d76b9d9ecfdb309e4555
mufichmj <mufichmj@rose-hulman.edu>
2018-03-08 01:11:44 -0500

    m6-DONE

--- src/m6_using_objects.py
+++ src/m6_using_objects.py
@@ -13,8 +13,10 @@ import rosegraphics as rg
 
 def main():
 
+    lines()
     circle_and_rectangle()
     two_circles()
+
     """ Calls the other functions to demonstrate and/or test them. """
     # Test your functions by putting calls to them here:
 
@@ -126,7 +128,7 @@ def circle_and_rectangle():
            150.0
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement this function, per its doc-string above.
+    # DONE: 3. Implement this function, per its doc-string above.
     #   -- ANY objects that meet the criteria are fine.
     # Put a statement in   main   to test this function
     #    (by calling this function).
@@ -137,6 +139,25 @@ def circle_and_rectangle():
 
 
 def lines():
+    window = rg.RoseWindow()
+    start = rg.Point(100, 50)
+    end = rg.Point(200, 30)
+    line = rg.Line(start, end)
+    line.color = 'blue'
+    line.thickness = 3
+    line.attach_to(window)
+
+    start = rg.Point(50, 500)
+    end = rg.Point(50, 30)
+    line2 = rg.Line(start, end)
+    line2.attach_to(window)
+    window.render()
+    midpoint = line.get_midpoint()
+    print(midpoint)
+    print(midpoint.x)
+    print(midpoint.y)
+
+    window.close_on_mouse_click()
     """
     -- Constructs a rg.RoseWindow.
     -- Constructs and draws on the window two rg.Lines such that:
@@ -159,7 +180,7 @@ def lines():
     -- Waits for the user to press the mouse, then closes the window.
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     # -------------------------------------------------------------------------
 
 
